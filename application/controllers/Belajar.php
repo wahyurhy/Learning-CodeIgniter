d9930738f0247290ee78fc9878a6f5624ba447fe
Wahyu Rahayu <wahyurahayu82@gmail.com>
2022-10-10 16:50:20 +0700

    init

---
+++ application/controllers/Belajar.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Belajar extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        echo "ini method index pada controller belajar";
+    }
+
+    public function halo() {
+        echo "ini method halo pada controller belajar";
+    }
+
+}
