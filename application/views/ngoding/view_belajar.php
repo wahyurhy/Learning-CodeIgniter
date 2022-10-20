70dcc53df4de4a0b01e82ac10281e36e8ce8baf4
Wahyu Rahayu <wahyurahayu82@gmail.com>
2022-10-20 16:07:30 +0700

    up to template

---
+++ application/views/ngoding/view_belajar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cara Membuat View Pada CodeIgniter</title>
+</head>
+<body>
+
+    <h1><?php echo $judul; ?></h1>
+    <h2>Ini adalah view view_belajar.php</h2>
+    <h3>Ini adalah view yang di tampilkan pada controller belajar, method halo dari tutorial <?php echo $tutorial; ?></h2>
+
+    <?php 
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 1);
+    echo br(2);
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 2);
+    echo img('https://asset.kompas.com/crops/YWI4canyjl4lMDXgcjiarnsVKDo=/84x49:5892x3921/750x500/data/photo/2022/03/09/6228977ff0914.jpg');
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 3);
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 4);
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 5);
+    echo heading("Helper HTML CodeIgniter | WahyuRahayu.com", 6);
+    ?>
+    
+</body>
+</html>
---
+++ application/views/v_footer.php
@@ -0,0 +1,4 @@
+            <footer><a href="https://github.com/wahyurhy/wahyurhy">WahyuRahayu</a></footer>
+        </div>
+    </body>
+</html>
